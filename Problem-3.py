1692622f6c8093127b77753bc62aa3c04de116bb
Marixm327 <marifat0330@gmail.com>
2025-12-18 15:25:07 +0530

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,5 +1,5 @@
 a = int(input("Enter a number: "))
-#The main logic is printing all odd numbers till a if a is an odd number and till a-1 if a is an even number
+
 output = a if a % 2 != 0 else a - 1 
 result = []
 
